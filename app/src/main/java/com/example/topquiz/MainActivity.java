e79cfc3c818463268b1fc6be60f59aad24b9218c
digitalatlantique <mick.info@yahoo.com>
2019-06-20 18:15:18 +0200

    Création de l'interface graphique principale et référencement des identifiants

---
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.topquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGreetingText = findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = findViewById(R.id.activity_main_name_input);
+        mPlayButton = findViewById(R.id.activity_main_play_btn);
+    }
+}
