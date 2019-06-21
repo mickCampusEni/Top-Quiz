925a5066bacb6194f9d5540395229f25d5eee078
digitalatlantique <mick.info@yahoo.com>
2019-06-21 16:06:03 +0200

    Implémentation de l'activité question

---
+++ app/src/main/java/com/example/topquiz/GameActivity.java
@@ -0,0 +1,29 @@
+package com.example.topquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    private TextView mQuestion;
+    private Button mAnswer1;
+    private Button mAnswer2;
+    private Button mAnswer3;
+    private Button mAnswer4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mQuestion = findViewById(R.id.activity_game_question_text);
+        mAnswer1 = findViewById(R.id.activity_game_answer1_btn);
+        mAnswer2 = findViewById(R.id.activity_game_answer2_btn);
+        mAnswer3 = findViewById(R.id.activity_game_answer3_btn);
+        mAnswer4 = findViewById(R.id.activity_game_answer4_btn);
+    }
+}
--- app/src/main/java/com/example/topquiz/MainActivity.java
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.topquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -49,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivityIntent);
             }
         });
     }
