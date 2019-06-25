a9c11ca28365e8a909889e7936c0f6955d88eb3c
digitalatlantique <mick.info@yahoo.com>
2019-06-25 15:18:44 +0200

    Ajout de question et amélioration de l'affichage de fin de jeu

--- app/src/main/java/com/example/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/example/topquiz/controller/GameActivity.java
@@ -89,8 +89,13 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setTitle("Well done !")
-                .setMessage("Your score is : " + mScore)
+        String title = (mScore >= 2 ? "Well done !" : "To improve !");
+        StringBuilder message = new StringBuilder("Your score is : ")
+                                    .append(mScore)
+                                    .append(" / ")
+                                    .append(4);
+        builder.setTitle(title)
+                .setMessage(message)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int witch){
@@ -116,30 +121,51 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private QuestionBank initializeQuestionBank(){
         QuestionBank questionBank = null;
 
-        Question question1 = new Question("Who is the creator of Android?",
-                Arrays.asList("Andy Rubin",
-                        "Steve Wozniak",
-                        "Jake Wharton",
-                        "Paul Smith"),
+        Question question1 = new Question("What is the name of the current french president?",
+                Arrays.asList("François Hollande", "Emmanuel Macron", "Jacques Chirac", "François Mitterand"),
+                1);
+
+        Question question2 = new Question("How many countries are there in the European Union?",
+                Arrays.asList("15", "24", "28", "32"),
+                2);
+
+        Question question3 = new Question("Who is the creator of the Android operating system?",
+                Arrays.asList("Andy Rubin", "Steve Wozniak", "Jake Wharton", "Paul Smith"),
                 0);
 
-        Question question2 = new Question("When did the first man land on the moon?",
-                Arrays.asList("1958",
-                        "1962",
-                        "1967",
-                        "1969"),
+        Question question4 = new Question("When did the first man land on the moon?",
+                Arrays.asList("1958", "1962", "1967", "1969"),
                 3);
 
-        Question question3 = new Question("What is the house number of The Simpsons?",
-                Arrays.asList("42",
-                        "101",
-                        "666",
-                        "742"),
+        Question question5 = new Question("What is the capital of Romania?",
+                Arrays.asList("Bucarest", "Warsaw", "Budapest", "Berlin"),
+                0);
+
+        Question question6 = new Question("Who did the Mona Lisa paint?",
+                Arrays.asList("Michelangelo", "Leonardo Da Vinci", "Raphael", "Carravagio"),
+                1);
+
+        Question question7 = new Question("In which city is the composer Frédéric Chopin buried?",
+                Arrays.asList("Strasbourg", "Warsaw", "Paris", "Moscow"),
+                2);
+
+        Question question8 = new Question("What is the country top-level domain of Belgium?",
+                Arrays.asList(".bg", ".bm", ".bl", ".be"),
+                3);
+
+        Question question9 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42", "101", "666", "742"),
                 3);
 
         questionBank = new QuestionBank(Arrays.asList(question1,
                                                         question2,
-                                                        question3));
+                                                        question3,
+                                                        question4,
+                                                        question5,
+                                                        question6,
+                                                        question7,
+                                                        question8,
+                                                        question9));
 
         return questionBank;
     }
